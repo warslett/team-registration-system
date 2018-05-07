401eadb2dd4ea8219acde758f5e55593a9e44d23
William Arslett <william.a@biffbangpow.com>
2018-05-07 17:49:47 +0100

    Added fontawesome, added footer, added user update controller (unimplemented)

---
+++ src/Controller/User/UserUpdateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\User;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserUpdateController
+{
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        // TODO: Implement User Update
+        return new Response("Unimplemented");
+    }
+}
