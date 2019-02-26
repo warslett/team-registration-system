5e8dd9811eb19c1ece00f715d13d0a8d63cea485
William Arslett <will@example.com>
2019-02-26 23:06:42 +0000

    Added team list

---
+++ src/Controller/Hike/HikeTeamsController.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Controller\Hike;
+
+
+use App\Factory\ResponseFactory;
+use App\Resolver\HikeResolver;
+use App\Service\CurrentUserService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HikeTeamsController
+{
+
+    /**
+     * @var HikeResolver
+     */
+    private $hikeResolver;
+
+    /**
+     * @var ResponseFactory
+     */
+    private $responseFactory;
+
+    /**
+     * @param HikeResolver $hikeResolver
+     * @param ResponseFactory $responseFactory
+     */
+    public function __construct(HikeResolver $hikeResolver, ResponseFactory $responseFactory)
+    {
+        $this->hikeResolver = $hikeResolver;
+        $this->responseFactory = $responseFactory;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \Twig_Error
+     */
+    public function __invoke(Request $request): Response
+    {
+        $hike = $this->hikeResolver->resolveById($request->get('hike_id'));
+        return $this->responseFactory->createTemplateResponse('hikes/teams.html.twig', [
+            'hike' => $hike,
+            'teams' => $hike->getTeams()
+        ]);
+    }
+}
